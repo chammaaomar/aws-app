b3e345185f68679ddfa6d72c3a417ff6514f92c0
Omar Chammaa <chammaa.omar@gmail.com>
2020-06-12 12:09:21 -0400

    codeBuild and codeDeploy specs

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -3,8 +3,22 @@
 STACK_NAME=awsbootstrap
 REGION=us-east-1
 CLI_PROFILE=default
+AWS_ACCOUNT_ID=`aws sts get-caller-idetntiy --profile $CLI_PROFILE --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
 
 EC2_INSTANCE_TYPE=t2.micro
+
+# Deploy static resources
+echo -e "\n\n========== Deploying setup.yml =========="
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME \
+  --template-profile setup.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 # Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml ==========="
